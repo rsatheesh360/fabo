06da8c7246a2bf9dd442851676e13d367fe89ee0
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-13 17:10:14 +0530

    fab

---
+++ .gitignore/fabo.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if(n <= 1):
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+n = int(input("Enter no of terms:"))
+print("Fibonacci sequence:")
+print("fibonacci sequence:")
+for i in range(n):
+    print (fibonacci(i))
+    print (fibonacci(i))
